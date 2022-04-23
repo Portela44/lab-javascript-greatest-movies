d4eb23ac9068be0ec4ecfea31be879a2f17cbd6a
Guillem <guillem.mart7@gmail.com>
2022-04-23 18:29:00 +0200

    iterations 2 and 3 done

--- src/movies.js
+++ src/movies.js
@@ -17,13 +17,35 @@ function getAllDirectors(movies) {
 console.log(getAllDirectors(movies));
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies,directorName,movieGenre) {
+  const resultArr = movies.filter(function (obj) {
+    return((obj.director===directorName) && (obj.genre.includes(movieGenre)))
+  })
+  return resultArr;
+}
+
+console.log(howManyMovies(movies,"Steven Spielberg", "Drama"));
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  allScores = [];
+  for(const obj of movies) {
+    allScores.push(obj.score);
+  }
+  const totalScore = allScores.reduce(function (acc,val) {
+    return acc + val;
+  });
+  return ((totalScore/250).toFixed(2));
+}
+
+console.log(scoresAverage(movies));
+
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
